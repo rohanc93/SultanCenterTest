bed145c7f5bb947b71fbb0ddc017038813a3df7c
rohan <rohan@techsevin.com>
2022-04-27 18:01:53 +0530

    smoke test completed

--- src/test/java/com/sultan/qa/testcases/SmokeTest.java
+++ src/test/java/com/sultan/qa/testcases/SmokeTest.java
@@ -18,13 +18,13 @@ import com.sultan.qa.pages.SearchPage;
 import com.sultan.qa.pages.TimeslotPage;
 
 public class SmokeTest extends TestBase {
-	LoginPage loginPage;
-	HomePage homePage;
-	CartPage cartPage;
-	SearchPage searchPage;
-	MiniCartPage miniCartPage;
-	TimeslotPage tsp;
-	PlaceOrderPage po;
+	LoginPage lp;
+	HomePage hp;
+	CartPage cp;
+	SearchPage sp;
+	MiniCartPage mp;
+	TimeslotPage tp;
+	PlaceOrderPage pp;
 	DeliveryPopUp dp;
 	
 	public SmokeTest() throws IOException {
@@ -36,60 +36,52 @@ public class SmokeTest extends TestBase {
 	public void setup() throws IOException {
 		initialization(prop.getProperty("isLogin")); // for signinurl1 use O as value , signinurl use 1
 		//System.out.println("isLogin : " + prop.getProperty("isLogin"));
-		loginPage = new LoginPage();
-		searchPage = new SearchPage();
-		cartPage = new CartPage();
-		homePage = new HomePage();
-		miniCartPage = new MiniCartPage();
-		po = new PlaceOrderPage();
-		tsp = new TimeslotPage();
+		lp = new LoginPage();
+		sp = new SearchPage();
+		cp = new CartPage();
+		hp = new HomePage();
+		mp = new MiniCartPage();
+		pp = new PlaceOrderPage();
+		tp = new TimeslotPage();
 		dp= new DeliveryPopUp();
 	}
 
 	@Test(priority = 1, enabled = true)
 	public void loggedInUserCheckout() throws IOException, InterruptedException {
-		loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		lp.login(prop.getProperty("username"), prop.getProperty("password"));
 		dp.selectCTCStore();
 		Thread.sleep(3000);
-		searchPage.search("milk");
+		sp.search("milk");
 		Thread.sleep(5000);
-		//cartPage.incQtyList();
-		searchPage.addToCart();
+		sp.addToCart();
 		Thread.sleep(3000);
-		miniCartPage.usecartIcon();
-		//cartPage.grandTotal();
+		mp.usecartIcon();
 		Thread.sleep(4000);
-		// cartPage.incQty();
-		// Thread.sleep(3000);
-		// cartPage.grandTotal1();
-		cartPage.clickProceedToCheckout();
+		cp.clickProceedToCheckout();
 		Thread.sleep(10000);
-		tsp.selectSlot();
-		po.selectPayment();
-		po.placeOrder();
-		po.orderSuccess();
-		//po.getOrderId();
+		tp.selectSlot();
+		pp.selectPayment();
+		pp.placeOrder();
+		pp.orderSuccess();
+
 	}
 
 	@Test(enabled = false)
 	public void guestUserCheckout() throws InterruptedException, IOException {
 		dp.selectDeliveryArea("salmiya");
-		searchPage.navigateUrl();
 		Thread.sleep(5000);
-		cartPage.incQtyList();
-		searchPage.addToCart();
+		sp.addToCart();
 		Thread.sleep(3000);
-		miniCartPage.usecartIcon();
-		cartPage.grandTotal();
+		mp.usecartIcon();
 		Thread.sleep(3000);
-		cartPage.clickProceedToCheckout();
-		loginPage.nonloggedInCheckout(prop.getProperty("username"), prop.getProperty("password"));
-		cartPage.clickProceedToCheckout();
+		cp.clickProceedToCheckout();
+		lp.nonloggedInCheckout(prop.getProperty("username"), prop.getProperty("password"));
+		cp.clickProceedToCheckout();
 		Thread.sleep(20000);
-		tsp.selectSlot();
-		po.placeOrder();
-		po.orderSuccess();
-		//po.getOrderId();
+		tp.selectSlot();
+		pp.selectPayment();
+		pp.placeOrder();
+		pp.orderSuccess();
 
 	}
 
