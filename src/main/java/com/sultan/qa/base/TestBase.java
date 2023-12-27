6998c1949a3e680aacf0cf3b35927252504eb665
rohan <rohan@techsevin.com>
2023-12-27 12:56:34 +0530

    Base File path changes

--- src/main/java/com/sultan/qa/base/TestBase.java
+++ src/main/java/com/sultan/qa/base/TestBase.java
@@ -68,27 +68,4 @@ public class TestBase {
 		}
 		
 	}
-	
-	
-	/*
-	 * public static void initialization1() { String browserName =
-	 * prop.getProperty("browser"); if (browserName.equals("chrome")) {
-	 * System.setProperty("webdriver.chrome.driver",
-	 * "/home/rohan/eclipse-workspace1/SultanCenter_Staging/src/main/resources/Browsers/chromedriver"
-	 * ); driver = new ChromeDriver(); } else if (browserName.equals("firefox")) {
-	 * System.setProperty("webdriver.gecko.driver",
-	 * "/home/rohan/eclipse-workspace1/SultanCenter_Staging/src/main/resources/Browsers/geckodriver"
-	 * ); System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,
-	 * "true"); System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,
-	 * "/home/rohan/eclipse-workspace1/SultanCenter_Staging/target/logs.txt");
-	 * driver = new FirefoxDriver();
-	 * 
-	 * } driver.manage().window().maximize(); driver.manage().deleteAllCookies();
-	 * 
-	 * driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT,
-	 * TimeUnit.SECONDS);
-	 * driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT,
-	 * TimeUnit.SECONDS); driver.get(prop.getProperty("signinurl1")); }
-	 */
-
 }
